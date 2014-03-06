2ca97d572838f4aa7fcfdf17d8a66378bfcdae07
Pawkette <sammy.morgan.james@gmail.com>
2014-03-06 06:14:53 +0000

    fix stuff

--- LibAnimation.lua
+++ LibAnimation.lua
@@ -3,11 +3,10 @@
 --
 -- @classmod LibAnimation
 -- @author Pawkette ( pawkette.heals@gmail.com )
--- @copyright 2014 Pawkette
 --[[
 The MIT License (MIT)
 
-Copyright (c) <year> <copyright holders>
+Copyright (c) 2014 Pawkette
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
